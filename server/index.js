1df5f192227ab250a85a126bdeaef7f18dc1b3aa
kdevenport <katie.l.devenport@gmail.com>
2019-03-16 13:45:28 -0600

    secondstepdone

--- server/index.js
+++ server/index.js
@@ -2,3 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = ('express-session');
 require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(session({
+    secret:ProcessingInstruction.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+const port = process.env.SERVER_PORT || 3000;
+app.listen( port, () => {
+    console.log(`Server listening on port ${port}.`);
+})
