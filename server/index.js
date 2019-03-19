cb1c13d8472144c60c8204f6afed7f2f51a505f2
kdevenport <katie.l.devenport@gmail.com>
2019-03-19 17:23:01 -0600

    stepsevencomplete

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 const checkForSession = require('./middlewares/checkForSession');
 
 const swag_controller = require('./controllers/swag_controller');
+const auth_controller = require('./controllers/auth_controller');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -18,6 +20,11 @@ app.use(session({
 app.use( checkForSession );
 
 app.get('./api/swag', swag_controller.read);
+
+app.post('/api/login', auth_controller.login);
+app.post('/api/register', auth_controller.register);
+app.post('/api/signout', auth_controller.signout);
+app.get('/api/user', auth_controller.getuser);
 
 const port = process.env.SERVER_PORT || 3000;
 app.listen( port, () => {
